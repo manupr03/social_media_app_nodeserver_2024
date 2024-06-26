9e511e85dedf5ae8e8ec723f4fe6cfb224d1b170
manupr03 <manuprajan03@gmail.com>
2024-06-26 07:06:35 +0530

    project setup

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -47,12 +47,102 @@ const addUser = (req,res)=>{
     })
 
 
+
+
+}
+
+const loginUser = async (req,res)=>{
+    const {email,password} = req.body
+
+    try{
+        readDB((err,db)=>{
+            if(err){
+                return res.status(500).json({
+                    status:0,
+                    message:"Database connection error"
+                })
+            }
+
+            const user = db.users.find(usr=>usr.email === email)
+
+            if(!user){
+                return res.status(200).json({
+                    status:0,
+                    message:'invalid email or password!'
+                })
+            }
+
+            const isPasswordValid = db.users.find(usr=>usr.password === password)
+
+            if(!isPasswordValid){
+                return res.status(200).json({
+                    status:0,
+                    message:'invalid email or password!'
+                })
+            }
+            
+            return res.status(200).json({
+                message:"Login successfull",
+                data:user
+                });
+
+
+
+        })
+
+    }catch(err){
+        return res.status(500).json({message:"Internal server error",error:err})
+    }
+   
+
+}
+
+const registerUser = (req,res)=>{
+    const { email } = req.body
+    readDB((err,db)=>{
+        if(err){
+            return res.status(500).json({
+                status:0,
+                message:"Database connection error"
+            })
+        }
+
+        const user = db.users.find(usr=>usr.email==email)
+        if(user){
+            return res.status(400).json({
+                status:0,
+                message:"An account with this email already exists"
+            })
+        }else{
+            db.users.push(req.body)
+            writeDB(db,(err,result)=>{
+                if(err){
+                    return res.status(500).json({
+                        status:0,
+                        message:"Database connection error"
+                    })
+                }
+        
+                return res.status(200).json({
+                    status:1,
+                    data:result
+                })
+            })
+        }
+
+        
+        
+
+        console.log('registerUser',user,!user)
+    })
 }
 
 
 
 module.exports = {
     getUsers,
-    addUser
+    addUser,
+    loginUser,
+    registerUser
 }
 
--- src/routers/userRoutes.js
+++ src/routers/userRoutes.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { getUsers,addUser } = require('../controllers/userControllers');
+const { getUsers,addUser,loginUser,registerUser } = require('../controllers/userControllers');
 
 
 router.get('/users',getUsers);
 router.post('/users',addUser);
+router.post('/users/login' ,loginUser)
+router.post('/users/register',registerUser)
 
 module.exports = router;
